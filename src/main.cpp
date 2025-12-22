e0023522ac3ae311a62db89f84e6e1f08eaf1dcc
RGC Exists <103580754+rgc-exists@users.noreply.github.com>
2025-12-21 22:31:53 -0500

    Minor optimization (as suggested by prevter)

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,35 @@
 using namespace geode::prelude;
 
 #include <Geode/modify/LevelEditorLayer.hpp>
-class $modify(LevelEditorLayer) {
-	GameObject* createObject(int p0, cocos2d::CCPoint p1, bool p2) {
-		GameObject* object = LevelEditorLayer::createObject(p0, p1, p2);
 
-		object->m_hasNoGlow = Mod::get()->getSettingValue<bool>("no-glow");
-		object->m_isDontFade = Mod::get()->getSettingValue<bool>("dont-fade");
-		object->m_isDontEnter = Mod::get()->getSettingValue<bool>("dont-enter");
+bool dontFade = true;
+bool dontEnter = true;
+bool noGlow = false;
+$execute{
+	dontFade = Mod::get()->getSettingValue<bool>("dont-fade");
+	listenForSettingChanges("dont-fade", [](bool value) {
+		dontFade = value;
+	});
 
-        return object;
-    }
+	dontEnter = Mod::get()->getSettingValue<bool>("dont-enter");
+	listenForSettingChanges("dont-enter", [](bool value) {
+		dontEnter = value;
+	});
+
+	noGlow = Mod::get()->getSettingValue<bool>("no-glow");
+	listenForSettingChanges("no-glow", [](bool value) {
+		noGlow = value;
+	});
+}
+
+class $modify(LevelEditorLayerHook, LevelEditorLayer) {
+	GameObject* createObject(int key, CCPoint position, bool noUndo) {
+		GameObject* object = LevelEditorLayer::createObject(key, position, noUndo);
+		
+		object->m_isDontFade = dontFade;
+		object->m_isDontEnter = dontEnter;
+		object->m_hasNoGlow = noGlow;
+
+		return object;
+	}
 };
