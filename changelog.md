d1fa6f8bddf874cf9200930bf2f2689ff425edef
kazido <beegee1803@gmail.com>
2026-03-14 18:42:36 -0400

    small optimization and future release support

--- changelog.md
+++ changelog.md
@@ -3,3 +3,7 @@
 
 # 1.0.1
 - Updated to support Geometry Dash v2.207
+
+# 1.0.1
+- Made small optimization to fetch settings once rather than at every placed object. Thanks @rgc-exists.
+- Updated to support Geometry Dash v2.2081 and future releases.
--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,38 @@
 using namespace geode::prelude;
 
 #include <Geode/modify/LevelEditorLayer.hpp>
-class $modify(LevelEditorLayer) {
-	GameObject* createObject(int p0, cocos2d::CCPoint p1, bool p2) {
-		GameObject* object = LevelEditorLayer::createObject(p0, p1, p2);
 
-		object->m_hasNoGlow = Mod::get()->getSettingValue<bool>("no-glow");
-		object->m_isDontFade = Mod::get()->getSettingValue<bool>("dont-fade");
-		object->m_isDontEnter = Mod::get()->getSettingValue<bool>("dont-enter");
+bool dontFade = false;
+bool dontEnter = false;
+bool noGlow = true;
+
+
+// Credit to @rgc-exists for the optimizations.
+$execute{
+	dontFade = Mod::get()->getSettingValue<bool>("dont-fade");
+	listenForSettingChanges("dont-fade", [](bool value) {
+		dontFade = value;
+	});
+
+		dontEnter = Mod::get()->getSettingValue<bool>("dont-enter");
+	listenForSettingChanges("dont-enter", [](bool value) {
+		dontEnter = value;
+	});
+
+		noGlow = Mod::get()->getSettingValue<bool>("no-glow");
+	listenForSettingChanges("no-glow", [](bool value) {
+		noGlow = value;
+	});
+}
+
+
+class $modify(LevelEditorLayerHook, LevelEditorLayer) {
+	GameObject* createObject(int key, cocos2d::CCPoint position, bool noUndo) {
+		GameObject* object = LevelEditorLayer::createObject(key, position, noUndo);
+
+		object->m_hasNoGlow = noGlow;
+		object->m_isDontFade = dontFade;
+		object->m_isDontEnter = dontEnter;
 
         return object;
     }
